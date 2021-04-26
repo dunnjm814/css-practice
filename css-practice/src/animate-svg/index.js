49e8d16ff597764da839d894e6f89d2cf274f981
Jason Dunn <dunnjm814@gmail.com>
2021-04-26 13:16:11 -0700

    parallax on animations page

--- css-practice/src/animate-svg/index.js
+++ css-practice/src/animate-svg/index.js
@@ -5,7 +5,20 @@ import './svganimate.css'
 export default function Svganimate() {
   return (
     <div className='animation-house'>
-      <BikeGuy />
+      <div className='wrapper'>
+        <div className='section parallax image1'>
+          <h1>Scroll down for an animation!</h1>
+        </div>
+        <div className='section static'>
+          <BikeGuy />
+        </div>
+        <div className='section parallax image2'>
+          <h1>Scroll down for an animation!</h1>
+        </div>
+        <div className='section static'>
+          <BikeGuy />
+        </div>
+      </div>
     </div>
   )
 }
